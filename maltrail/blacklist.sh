99b5e4063c948cecf84194c8b5e757ff9ddf6a47
ram nezer <98687653+ramner98@users.noreply.github.com>
2022-08-26 02:32:19 -0400

    Update blacklist.sh

--- maltrail/blacklist.sh
+++ maltrail/blacklist.sh
@@ -8,9 +8,9 @@ user=$(echo $(users) | cut -d ' ' -f 1)
 echo $(cat $ipset_log) >> /home/$user/Desktop/maltrail-found-$(date +'%Y-%m-%d').log
 echo $(cat $ipset_log) >> /root/maltrail-found-$(date +'%Y-%m-%d').log
 
-printf  "\n############################################\nfound malicious traffic check with\n[sudo ipset list | grep -F 'ip address']\nif the malicious addresses have been blocked\n############################################\n" >> /home/$user/Desktop/maltrail-found-$(date +'%Y-%m-%d').log
+printf  "\n#############################################################\nfound malicious traffic check with\n[sudo ipset list blacklists && sudo ipset list blacklists2]\nif the malicious addresses have been blocked\n#############################################################\n" >> /home/$user/Desktop/maltrail-found-$(date +'%Y-%m-%d').log
 
-printf  "\n############################################\nfound malicious traffic check with\n[sudo ipset list | grep -F 'ip address']\nif the malicious addresses have been blocked\n############################################\n" >> /root/maltrail-found-$(date +'%Y-%m-%d').log
+printf  "\n#############################################################\nfound malicious traffic check with\n[sudo ipset list blacklists && sudo ipset list blacklists2]\nif the malicious addresses have been blocked\n#############################################################\n" >> /root/maltrail-found-$(date +'%Y-%m-%d').log
 
 
 for S in ${DIRTOSCAN}; do
@@ -22,7 +22,9 @@ sudo ipset add blacklists "$S" 2> /dev/null
 # ipv6
 sudo ipset add blacklists2 "$S" 2> /dev/null
 
-sudo -i ipset save > /etc/ipset.conf
+sudo -i ipset save blacklists > /etc/ipset_maltrail.conf
+sudo -i ipset save blacklists2 > /etc/ipset_maltrail2.conf
+
 sudo echo "(malware)" >> /opt/auto-clamIPS/notify-clamMA/logs/notify2.log
 
 done
