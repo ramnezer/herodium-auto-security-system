d57758d1a81ba0a26779c7910e4129f3da3ade5a
ram nezer <98687653+ramner98@users.noreply.github.com>
2022-08-26 02:30:56 -0400

    Update listener_maltrail.sh

--- maltrail/listener_maltrail.sh
+++ maltrail/listener_maltrail.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo -i ipset restore -f -! /etc/ipset.conf
+sudo -i ipset restore -f -! /etc/ipset_maltrail.conf
+sudo -i ipset restore -f -! /etc/ipset_maltrail2.conf
+
+
+sudo ipset create blacklists hash:ip timeout 0 maxelem 150000
+sudo ipset create blacklists2 hash:ip timeout 0 family inet6 maxelem 150000
 
 sudo iptables -C INPUT  -m set  --match-set blacklists  src -j DROP 2> /dev/null || sudo iptables -I INPUT 1  -m set  --match-set blacklists src -j  DROP 2> /dev/null
 sudo ip6tables -C INPUT  -m set  --match-set blacklists2  src -j DROP 2> /dev/null || sudo ip6tables -I INPUT 1  -m set  --match-set blacklists2 src -j DROP 2> /dev/null
