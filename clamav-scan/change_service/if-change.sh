f02fd69295646ad61aa0b8522737df771b6dab8e
ram nezer <98687653+ramner98@users.noreply.github.com>
2022-09-22 06:51:16 -0400

    Update if-change.sh

--- clamav-scan/change_service/if-change.sh
+++ clamav-scan/change_service/if-change.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 10
+
 exc=$(echo "'|.cache|.mozilla|.local|.config|.gnupg|snap/\.|auto-clamIPS/VIRUS-FOUND/|'")
 
 sudo -i inotifywait  -ecreate,move -m -r -q --format  '%Xe %w%f' --exclude $exc /home/ >> '/opt/auto-clamIPS/auto-clamav/logs/change.log'
