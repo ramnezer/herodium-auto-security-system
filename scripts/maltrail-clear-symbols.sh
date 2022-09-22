92435a7084463473cdbe014fa2918e983041adbf
ram nezer <98687653+ramner98@users.noreply.github.com>
2022-09-22 06:58:10 -0400

    Add files via upload

---
+++ scripts/maltrail-clear-symbols.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# make sure that the value does not come with unnecessary symbols
+# this command will be repeated several times to make sure the process was successful
+
+sudo seq 3 | xargs -I -- sudo sed -i -e 's/^"//' -i -e 's/"$//' -i -e 's/^(//' -i -e 's/)$//' /opt/auto-clamIPS/maltrail/logs/clean2.log
+
+sudo cat /opt/auto-clamIPS/maltrail/logs/clean2.log | tr -d '""()' >> /opt/auto-clamIPS/maltrail/logs/clean3.log
+
