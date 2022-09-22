90df87769e65e2b4c8dd0998d90d0a6526740f93
ram nezer <98687653+ramner98@users.noreply.github.com>
2022-09-22 06:54:54 -0400

    Update maltrail_scan.py

--- maltrail/maltrail_scan.py
+++ maltrail/maltrail_scan.py
@@ -10,28 +10,48 @@ def change_commands():
 
  if int(check_malware.returncode) == 0:
   subprocess.run(['sudo', 'systemctl', 'stop', 'maltrail_scan.timer'])
+ 
   subprocess.run(
       "sudo grep -E '(malware)|(malicious)' /opt/auto-clamIPS/maltrail/logs/scan.log >> /opt/auto-clamIPS/maltrail/logs/clean.log", shell=True)
+ 
   subprocess.run(
       "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/scan.log", shell=True)
+ 
   subprocess.run(
-      "sudo cut -d ' ' -f 10 /opt/auto-clamIPS/maltrail/logs/clean.log >> /opt/auto-clamIPS/maltrail/logs/clean2.log", shell=True)
+      "sudo cut -d ' ' -f 10 /opt/auto-clamIPS/maltrail/logs/clean.log >> /opt/auto-clamIPS/maltrail/logs/clean1.log", shell=True)
+  
   subprocess.run(
-      "sudo sort -u /opt/auto-clamIPS/maltrail/logs/clean2.log >> /opt/auto-clamIPS/maltrail/logs/ipset.log", shell=True)
+      "sudo cut -d ' ' -f 1 /opt/auto-clamIPS/maltrail/logs/clean1.log >> /opt/auto-clamIPS/maltrail/logs/clean2.log", shell=True)
+  
+  subprocess.run(['sudo', 'bash', '/opt/auto-clamIPS/maltrail/maltrail-clear-symbols.sh'])
+
+  subprocess.run(
+      "sudo sort -u /opt/auto-clamIPS/maltrail/logs/clean3.log >> /opt/auto-clamIPS/maltrail/logs/ipset.log", shell=True)
+  
   subprocess.run(
       "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/clean.log", shell=True)
+  
+  subprocess.run(
+      "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/clean1.log", shell=True)
+  
   subprocess.run(
       "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/clean2.log", shell=True)
+
+  subprocess.run(
+      "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/clean3.log", shell=True)
+
   subprocess.run(
       "sudo bash '/opt/auto-clamIPS/maltrail/blacklist.sh'", shell=True)
+  
   subprocess.run(
       "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/ipset.log", shell=True)
+
 ### To avoid duplicates while scanning url we will perform double clean to scan log
   subprocess.run(
       "sudo -i truncate -s 0 /opt/auto-clamIPS/maltrail/logs/scan.log", shell=True)
+ 
   subprocess.run(['sudo', 'systemctl', 'start', 'maltrail_scan.timer'])
 
 
 change_commands()
-
 
