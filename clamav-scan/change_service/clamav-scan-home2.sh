47cafac807c46e1c3303a1754b4556dae60b6015
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-01-05 14:54:59 -0500

    Update clamav-scan-home2.sh

--- clamav-scan/change_service/clamav-scan-home2.sh
+++ clamav-scan/change_service/clamav-scan-home2.sh
@@ -5,6 +5,7 @@ LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log";
 LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/real-time-history-$(date +'%Y-%m-%d').log";
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify.log"
 exclude=$(echo $(cat "/opt/auto-clamIPS/auto-clamav/ignore_list.txt"))
+option_move=$(echo "")
 DIRTOSCAN="/home/"
 user=$(echo $(users) | cut -d ' ' -f 1)
 for S in ${DIRTOSCAN}; do
@@ -13,7 +14,7 @@ echo "Starting a scan of "$S" directory.
 Amount of data to be scanned is "$DIRSIZE"."
 mkdir -p /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/VIRUS-FOUND/
 chown -R $(echo $(users) | cut -d ' ' -f 1):$(echo $(users) | cut -d ' ' -f 1) /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/
-clamscan --infected --recursive --exclude=/home/$user/auto-clamIPS/VIRUS-FOUND/ --exclude=/home/$user/.local/share/Trash/files/ --exclude=/home/$user/Desktop/clamav-found-malware-$(date +'%Y-%m-%d').log $exclude "$S" >> "$LOGFILE" 
+clamscan --infected --recursive --exclude=/home/$user/auto-clamIPS/VIRUS-FOUND/ --exclude=/home/$user/.local/share/Trash/files/ --exclude=/home/$user/Desktop/clamav-found-malware-$(date +"%Y-%m-%d").log $exclude $option_move "$S" >> "$LOGFILE"
 zip -m -j /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/VIRUS-FOUND/VIRUS-FOUND.zip /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/VIRUS-FOUND/* 1> /dev/null
 mkdir -p /opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/
 cat "$LOGFILE" >> "$LOGS_HISTORY"
