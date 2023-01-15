6c84f36fac6e054de77788c4a312d56ca71c30c9
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-01-15 15:48:05 +0200

    Update clamav-root-scan.sh

--- clamav-scan/clamav-root-scan.sh
+++ clamav-scan/clamav-root-scan.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /var/log/clamav/
 rm -rf "/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log" 
 LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log";
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify1.log"
-LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/root-scan-history/root-sacn-history-$(date +'%Y-%m-%d').log"
+LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/root-scan-history/root-scan-history-$(date +'%Y-%m-%d').log"
 exclude=$(echo $(cat "/opt/auto-clamIPS/auto-clamav/ignore_list.txt"))
 DIRTOSCAN="/"
 user=$(echo $(users) | cut -d ' ' -f 1)
@@ -12,8 +12,8 @@ for S in ${DIRTOSCAN}; do
 DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1)
 echo "Starting a scan of "$S" directory.
 Amount of data to be scanned is "$DIRSIZE"."
-clamscan --infected --detect-pua=yes --recursive $exclude "$S" >> "$LOGFILE"
-mkdir -p /opt/auto-clamIPS/auto-clamav/logs/logs_history/root-sacn-history/
+clamscan --infected --recursive $exclude "$S" >> "$LOGFILE"
+mkdir -p /opt/auto-clamIPS/auto-clamav/logs/logs_history/root-scan-history/
 cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
 # get the value of "Infected lines"
