4b0d060d1af9b9ca42c8fdb8ff3433473bfad718
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-02-28 20:09:57 +0200

    Update listener_maltrail.sh

--- maltrail/listener_maltrail.sh
+++ maltrail/listener_maltrail.sh
@@ -11,5 +11,11 @@ sudo iptables -C INPUT  -m set  --match-set blacklists  src -j DROP 2> /dev/null
 sudo ip6tables -C INPUT  -m set  --match-set blacklists2  src -j DROP 2> /dev/null || sudo ip6tables -I INPUT 1  -m set  --match-set blacklists2 src -j DROP 2> /dev/null
 
 sudo touch /var/log/maltrail/$(date +"%Y-%m-%d").log
+
 ### To avoid an unnecessary warning after system reboot tail will only listen to new results from the target log file
-sudo -i tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log >>'/opt/auto-clamIPS/maltrail/logs/scan.log'
+timeout  10m  sudo -i tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log >>'/opt/auto-clamIPS/maltrail/logs/scan.log'
+
+
+sudo bash /opt/auto-clamIPS/maltrail/maltrail_fix_listener.sh & sudo bash /opt/auto-clamIPS/maltrail/maltrail_fix_service.sh
+ 
+#############################################################################################################################
