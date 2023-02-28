3d8b871669da77b961534f04b26151397eed839a
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-02-28 20:14:22 +0200

    Update clamav-scan-home2.sh

--- clamav-scan/change_service/clamav-scan-home2.sh
+++ clamav-scan/change_service/clamav-scan-home2.sh
@@ -21,7 +21,7 @@ cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
 # get the value of "Infected lines"
 MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3)
-# if the value is not equal to zero, send log to desktop and root
+# 
 if [ "$MALWARE" -ne "0" ];then
 # using file below
 
