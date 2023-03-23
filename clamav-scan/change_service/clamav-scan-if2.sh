7f958da1ebd4f196e17d33563dcae0110e895c6e
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-03-23 16:40:32 +0200

    Update clamav-scan-if2.sh

--- clamav-scan/change_service/clamav-scan-if2.sh
+++ clamav-scan/change_service/clamav-scan-if2.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-sudo mkdir -p /var/log/clamav/
+mkdir -p /var/log/clamav/
 LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log"
 LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/real-time-history-$(date +'%Y-%m-%d').log"
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify.log"
 user=$(echo $(users) | cut -d ' ' -f 1)
 
+### For distributions with non-english languages,get the correct desktop name 
+### and convert it to universal(for english and non-english distro)variable
+desktop1=$(cat /home/$user/.config/user-dirs.dirs | grep "XDG_DESKTOP_DIR" | cut -d'/' -f2- | tr -d '"')
+
 cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
-cat $LOGFILE >> /home/$user/Desktop/clamav-found-malware-$(date +'%Y-%m-%d').log
+cat $LOGFILE >> "/home/$user/$desktop1/clamav-found-malware-$(date +'%Y-%m-%d').log"
 cat $LOGFILE >> /root/clamav-found-malware-$(date +'%Y-%m-%d').log
-rm -rf "/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log" 
-
-
+rm -rf "/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log"
