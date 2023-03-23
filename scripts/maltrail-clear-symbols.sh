a9ff8da182a18e86abecc32f82e6cd793fb2be61
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-03-23 16:18:54 +0200

    Update maltrail-clear-symbols.sh

--- scripts/maltrail-clear-symbols.sh
+++ scripts/maltrail-clear-symbols.sh
@@ -3,7 +3,6 @@
 # make sure that the value does not come with unnecessary symbols
 # this command will be repeated several times to make sure the process was successful
 
-sudo seq 3 | xargs -I -- sudo sed -i -e 's/^"//' -i -e 's/"$//' -i -e 's/^(//' -i -e 's/)$//' /opt/auto-clamIPS/maltrail/logs/clean2.log
+seq 3 | xargs -I -- sudo sed -i -e 's/^"//' -i -e 's/"$//' -i -e 's/^(//' -i -e 's/)$//' /opt/auto-clamIPS/maltrail/logs/clean2.log
 
-sudo cat /opt/auto-clamIPS/maltrail/logs/clean2.log | tr -d '""()' >> /opt/auto-clamIPS/maltrail/logs/clean3.log
-
+cat /opt/auto-clamIPS/maltrail/logs/clean2.log | tr -d '""()' >> /opt/auto-clamIPS/maltrail/logs/clean3.log
