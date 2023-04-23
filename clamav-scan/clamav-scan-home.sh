ac321b603fc646d5d532239cc7d1b48bf0e4a593
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-04-23 15:47:05 +0300

    Update clamav-scan-home.sh

--- clamav-scan/clamav-scan-home.sh
+++ clamav-scan/clamav-scan-home.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-python3 /opt/auto-clamIPS/notify-clamMA/notify-reset-boot.py
+/opt/auto-clamIPS/notify-clamMA/check_user.sh
 mkdir -p /var/log/clamav/
-rm -rf "/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log" 
+rm -f "/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log" 
 LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log"
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify1.log"
 LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/home-scan-history/home-scan-history-$(date +'%Y-%m-%d').log"
-exclude=$(echo $(cat "/opt/auto-clamIPS/auto-clamav/ignore_list.txt"))
 DIRTOSCAN="/home/ /root/ /boot/ /etc/ /opt/ /dev/"
-user=$(echo $(users) | cut -d ' ' -f 1)
+user=$( users | cut -d ' ' -f 1)
 
 ### For distributions with non-english languages,get the correct desktop name 
 ### and convert it to universal(for english and non-english distro)variable
-desktop1=$(cat /home/$user/.config/user-dirs.dirs | grep "XDG_DESKTOP_DIR" | cut -d'/' -f2- | tr -d '"')
+desktop1=$(cat /home/"$user"/.config/user-dirs.dirs | grep "XDG_DESKTOP_DIR" | cut -d'/' -f2- | tr -d '"')
 
 for S in ${DIRTOSCAN}; do
 DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1)
-echo "Starting a scan of "$S" directory.
-Amount of data to be scanned is "$DIRSIZE"."
-clamscan --infected --recursive $exclude "$S" >> "$LOGFILE"
+
+
+clamdscan --fdpass --infected "$S" >> "$LOGFILE"
 mkdir -p /opt/auto-clamIPS/auto-clamav/logs/logs_history/home-scan-history/
 cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
@@ -30,7 +29,7 @@ if [ "$MALWARE" -ne "0" ];then
 cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
 cp $LOGFILE "/home/$user/$desktop1/"
-cp $LOGFILE /root/
+cp $LOGFILE "/root/"
 
 
 fi
