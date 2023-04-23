a9f8da01cd1096b108edc148fec22ef7fa3ef908
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-04-23 15:38:18 +0300

    Update maltrail_fix_listener.sh

--- scripts/maltrail_fix_listener.sh
+++ scripts/maltrail_fix_listener.sh
@@ -4,18 +4,12 @@
 ### fix error 'listener frozen/not responding'
 ##########################################################################################################################
 
+data=$(date +"%Y-%m-%d")
+
 # Prevent the listener from entering to a frozen state by creating a loop
 while
  
- touch /var/log/maltrail/$(date +"%Y-%m-%d").log
- timeout  10m  sudo -i tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log >>'/opt/auto-clamIPS/maltrail/logs/scan.log'
-
-### Prevent 'zombie' processes if it is true
- kill -9 $(pgrep -f  "tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log")
- kill -9 $(pgrep -f  "timeout  10m  sudo -i tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log")
- kill -9 $(pgrep -f  "timeout  10m  tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log")
- kill -9 $(pgrep -f  "sudo -i tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log")
- kill -9 $(pgrep -f  "timeout  10m  sudo -i tail -n0 -f /var/log/maltrail/$(date +"%Y-%m-%d").log")
+ timeout 10m tail -n0 -f /var/log/maltrail/"$data".log >>'/opt/auto-clamIPS/maltrail/logs/scan.log'
  
  sleep 1
  
