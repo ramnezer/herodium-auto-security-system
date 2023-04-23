ea1f8594778e0298b473519de7969bd9297f83e1
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-04-23 15:54:21 +0300

    Update clamav-scan-home2.sh

--- clamav-scan/change_service/clamav-scan-home2.sh
+++ clamav-scan/change_service/clamav-scan-home2.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 mkdir -p /var/log/clamav/
-python3 /opt/auto-clamIPS/notify-clamMA/notify-reset-boot.py
+/opt/auto-clamIPS/notify-clamMA/check_user.sh
 LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log";
 LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/real-time-history-$(date +'%Y-%m-%d').log";
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify.log"
-exclude=$(echo $(cat "/opt/auto-clamIPS/auto-clamav/ignore_list.txt"))
-option_move=$(echo "")
+option_remove=$(echo "")
 DIRTOSCAN="/home/"
-user=$(echo $(users) | cut -d ' ' -f 1)
+user=$( users | cut -d ' ' -f 1)
 
 ### For distributions with non-english languages,get the correct desktop name 
 ### and convert it to universal(for english and non-english distro)variable
-desktop1=$(cat /home/$user/.config/user-dirs.dirs | grep "XDG_DESKTOP_DIR" | cut -d'/' -f2- | tr -d '"')
+desktop1=$(cat /home/"$user"/.config/user-dirs.dirs | grep "XDG_DESKTOP_DIR" | cut -d'/' -f2- | tr -d '"')
 
 for S in ${DIRTOSCAN}; do
 DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1)
-echo "Starting a scan of "$S" directory.
-Amount of data to be scanned is "$DIRSIZE"."
-mkdir -p /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/VIRUS-FOUND/
-chown -R $(echo $(users) | cut -d ' ' -f 1):$(echo $(users) | cut -d ' ' -f 1) /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/
-clamscan --infected --recursive --exclude=/home/$user/auto-clamIPS/VIRUS-FOUND/ --exclude=/home/$user/.local/share/Trash/files/ --exclude=/home/$user/$desktop1/clamav-found-malware-$(date +"%Y-%m-%d").log $exclude $option_move "$S" >> "$LOGFILE"
-zip -m -j /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/VIRUS-FOUND/VIRUS-FOUND.zip /home/$(echo $(users) | cut -d ' ' -f 1)/auto-clamIPS/VIRUS-FOUND/* 1> /dev/null
+
+
+clamdscan --fdpass --infected $option_remove "$S" >> "$LOGFILE" 
 mkdir -p /opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/
 cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
@@ -33,7 +29,7 @@ if [ "$MALWARE" -ne "0" ];then
 cat "$LOGFILE" >> "$LOGS_HISTORY"
 cat "$LOGFILE" >> "$LOGFILE2"
 cat $LOGFILE >> "/home/$user/$desktop1/clamav-found-malware-$(date +'%Y-%m-%d').log"
-cat $LOGFILE >> /root/clamav-found-malware-$(date +'%Y-%m-%d').log
+cat $LOGFILE >> "/root/clamav-found-malware-$(date +'%Y-%m-%d').log"
 
 
 fi
