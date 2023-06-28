7a4b4e7077487bba0b5faa34b81c24a0825262ce
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-06-28 04:16:30 +0300

    Update cpulimit-chack.sh

--- clamav-scan/change_service/cpulimit-chack.sh
+++ clamav-scan/change_service/cpulimit-chack.sh
@@ -8,17 +8,28 @@ sleep 1
 
 while
  
- sleep 1
  check=$(pgrep -f  "/usr/sbin/clamd")
- check_cpu=$(pgrep -f  "cpulimit -l 50 -p")
- if [ "$check" ] && [ "$check_cpu" == "" ]
+ check_cpu=$(pgrep -f  "cpulimit -l 25 -p $check")
+ check_cpu3=$(pgrep -f  "cpulimit -l 75 -p $check")
+ check_root=$(pgrep -f  "clamdscan --fdpass --infected /")
+
+ if [ "$check" ] && [ "$check_cpu" == "" ] && [ "$check_root" == "" ]
  then
- cpulimit -l 50 -p $check
- sleep 2
+ 
+ cpulimit -l 25 -p $check
+ 
+ elif [ "$check" ] && [ "$check_cpu3" == "" ] && [ "$check_root" ]
+ then
+  
+ cpulimit -l 75 -p $check
+ 
 
  fi
+
+sleep 1
 
 do
     :
 done
+
 
