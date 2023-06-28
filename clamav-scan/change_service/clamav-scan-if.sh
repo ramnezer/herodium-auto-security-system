c5a05016a057cfe754fa12bf568a81f2c6fc7f97
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-06-28 04:18:00 +0300

    Update clamav-scan-if.sh

--- clamav-scan/change_service/clamav-scan-if.sh
+++ clamav-scan/change_service/clamav-scan-if.sh
@@ -5,15 +5,15 @@ rm -f "/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log"
 LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log"
 LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/real-time-history-$(date +'%Y-%m-%d').log"
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify.log"
-user=$( users | cut -d ' ' -f 1)
+user=$(cat /etc/group | grep $(id -u $(w -s | grep "tty7" | cut -d ' ' -f 1)) | cut -d: -f1)
 option_remove=$(echo "")
 
 ### For distributions with non-english languages,get the correct desktop name 
 ### and convert it to universal(for english and non-english distro)variable
 desktop1=$(cat /home/$user/.config/user-dirs.dirs | grep "XDG_DESKTOP_DIR" | cut -d'/' -f2- | tr -d '"')
 
-clamdscan --fdpass --infected $option_remove --file-list="/opt/auto-clamIPS/auto-clamav/logs/auto.log" >> "$LOGFILE"
+clamdscan --fdpass --infected --remove --file-list="/opt/auto-clamIPS/auto-clamav/logs/auto.log" >> "$LOGFILE" 
 mkdir -p /opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/
 
-cat "$LOGFILE" >> "$LOGS_HISTORY"
+
 cat "$LOGFILE" >> "$LOGFILE2"
