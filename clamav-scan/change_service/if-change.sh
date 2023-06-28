2cd86b7713f1bfeb212dd5d56b97d90b2dc26414
ram nezer <98687653+ramner98@users.noreply.github.com>
2023-06-28 04:13:04 +0300

    Update if-change.sh

--- clamav-scan/change_service/if-change.sh
+++ clamav-scan/change_service/if-change.sh
@@ -2,4 +2,9 @@
 
 sleep 10
 
-/opt/auto-clamIPS/auto-clamav/change-li.sh & /opt/auto-clamIPS/auto-clamav/cpulimit-chack.sh
+/opt/auto-clamIPS/auto-clamav/change-li.sh /&
+/opt/auto-clamIPS/auto-clamav/media_scan/media_if-change.sh /&
+/opt/auto-clamIPS/auto-clamav/media_scan/check_media.sh /&
+/opt/auto-clamIPS/auto-clamav/cpulimit-chack.sh /&
+/opt/auto-clamIPS/auto-clamav/integral_cpulimit.sh /&
+/opt/auto-clamIPS/auto-clamav/cpulimit_inotifywait.sh
