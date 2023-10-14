067d20e3c758945514af4751bd402d477d4e5b42
ram nezer <98687653+ramnezer@users.noreply.github.com>
2023-10-15 00:57:29 +0300

    Update cpulimit_inotifywait.sh

--- clamav-scan/change_service/cpulimit_inotifywait.sh
+++ clamav-scan/change_service/cpulimit_inotifywait.sh
@@ -7,12 +7,12 @@ sleep 1
 while
  
  inotifywait=$(pgrep -f  "inotifywait")
- inotifywait_cpu=$(pgrep -f  "cpulimit -l 15 -p $inotifywait")
+ inotifywait_cpu=$(pgrep -f  "cpulimit -l 30 -p $inotifywait")
 
  if [ "$inotifywait" ] && [ "$inotifywait_cpu" == "" ]
  then
  
- cpulimit -l 15 -p $inotifywait
+ cpulimit -l 30 -p $inotifywait
   
  fi
 
