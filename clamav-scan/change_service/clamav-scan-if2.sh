3eeb62ce6cf151b2c15eed19a2d15c2b276543d2
ram nezer <98687653+ramnezer@users.noreply.github.com>
2023-10-15 00:56:36 +0300

    Update clamav-scan-if2.sh

--- clamav-scan/change_service/clamav-scan-if2.sh
+++ clamav-scan/change_service/clamav-scan-if2.sh
@@ -4,7 +4,7 @@ mkdir -p /var/log/clamav/
 LOGFILE="/var/log/clamav/clamav-found-malware-$(date +'%Y-%m-%d').log"
 LOGS_HISTORY="/opt/auto-clamIPS/auto-clamav/logs/logs_history/real-time-history/real-time-history-$(date +'%Y-%m-%d').log"
 LOGFILE2="/opt/auto-clamIPS/notify-clamMA/logs/notify.log"
-user=$(cat /etc/group | grep $(id -u $(w -s | grep "tty7" | cut -d ' ' -f 1)) | cut -d: -f1)
+user=$( users | cut -d ' ' -f 1)
 
 ### For distributions with non-english languages,get the correct desktop name 
 ### and convert it to universal(for english and non-english distro)variable
