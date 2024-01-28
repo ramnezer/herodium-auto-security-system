20d310a6f377175fdff4fe15667adf1e66e78991
Ram Nezer <rncv909090@gmail.com>
2024-01-28 17:22:22 +0200

    Update media-li.sh

--- clamav-scan/media_scan/media-li.sh
+++ clamav-scan/media_scan/media-li.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+sysctl fs.inotify.max_user_instances=8192
 sysctl fs.inotify.max_user_watches=524288
 
-inotifywait  -ecreate,move,close_write -m -r -q  --format  '%Xe %w%f' /media/  >> '/opt/auto-clamIPS/auto-clamav/logs/change.log'
-
-
+inotifywait  -ecreate,attrib -m -r -q  --format  '%Xe %w%f' /media/  >> '/opt/auto-clamIPS/auto-clamav/logs/change.log' /&
+inotifywait  -emove,close_write -m -r -q  --format  '%Xe %w%f' /media/  >> '/opt/auto-clamIPS/auto-clamav/logs/change_entangl.log'
