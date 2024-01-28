96fd3ca125e4d77f159d92d07684f3e54bdd3c77
Ram Nezer <rncv909090@gmail.com>
2024-01-28 17:23:39 +0200

    Update change-li.sh

--- clamav-scan/change_service/change-li.sh
+++ clamav-scan/change_service/change-li.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sysctl fs.inotify.max_user_instances=8192
 sysctl fs.inotify.max_user_watches=524288
 
 data=$(cat /opt/auto-clamIPS/auto-clamav/logs/data_scan.log)
 
-inotifywait  -ecreate,move,close_write -m -r -q  --format  '%Xe %w%f' $data  >> '/opt/auto-clamIPS/auto-clamav/logs/change.log'
+inotifywait  -ecreate,attrib -m -r -q  --format  '%Xe %w%f' $data  >> '/opt/auto-clamIPS/auto-clamav/logs/change.log' /&
+inotifywait  -emove,close_write -m -r -q  --format  '%Xe %w%f' $data  >> '/opt/auto-clamIPS/auto-clamav/logs/change_entangl.log'
